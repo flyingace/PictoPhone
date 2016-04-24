5dc240bc69f688d222c25ad97db4056dd4a120c5
David Cameron <dc@davidcameron.biz>
2016-04-24 00:37:56 -0400

    Adjusted basic formatting in files

--- app/store/configureStore.production.js
+++ app/store/configureStore.production.js
@@ -1,7 +1,7 @@
-import { createStore, applyMiddleware } from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
-import { hashHistory } from 'react-router';
-import { routerMiddleware } from 'react-router-redux';
+import {hashHistory} from 'react-router';
+import {routerMiddleware} from 'react-router-redux';
 import rootReducer from '../reducers';
 
 const router = routerMiddleware(hashHistory);
@@ -9,5 +9,5 @@ const router = routerMiddleware(hashHistory);
 const enhancer = applyMiddleware(thunk, router);
 
 export default function configureStore(initialState) {
-  return createStore(rootReducer, initialState, enhancer);
+    return createStore(rootReducer, initialState, enhancer);
 }
