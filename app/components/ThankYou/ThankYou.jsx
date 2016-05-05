07f062fe6d068a9f1b120ee07f1b2a7670d6a614
David Cameron <dc@davidcameron.biz>
2016-05-04 22:39:40 -0400

    Added keyboard but code isn't working yet

--- app/components/ThankYou/ThankYou.jsx
+++ app/components/ThankYou/ThankYou.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './ThankYou.scss';
 import Key from '../Key/Key.jsx';
+import Keyboard from '../Keyboard/Keyboard.jsx';
 
 /**
  * ThankYou class.
@@ -33,9 +34,7 @@ const ThankYou = React.createClass(/** @lends ThankYou.prototype */{
               <h1>Thank You</h1>
                 <p>You're done! Check your email later for a link to the completed game!</p>
                 <Key letterValue="A" />
-                <Key letterValue="S" />
-                <Key letterValue="D" />
-                <Key letterValue="F" />
+                <Keyboard />
             </div>
         );
     }
