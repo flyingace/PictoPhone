56525ce916f1247d414f2be89a11cd97ff67e949
Lei <sportzhulei@gmail.com>
2016-05-09 15:20:32 -0400

    Fix initial state for nameList and format cleaning

--- app/actions/welcome.js
+++ app/actions/welcome.js
@@ -7,26 +7,26 @@ export const RECEIVE_WELCOME_DATA = 'RECEIVE_WELCOME_DATA';
 export const FAILURE_WELCOME_DATA = 'FAILURE_WELCOME_DATA';
 
 export function requestWelcomeData() {
-	return { type: REQUEST_WELCOME_DATA};
+    return { type: REQUEST_WELCOME_DATA};
 }
 
 export function receiveWelcomeData(data) {
-	return { type: RECEIVE_WELCOME_DATA, state: data }
+    return { type: RECEIVE_WELCOME_DATA, state: data }
 }
 
 export function failureWelcomeData() {
-	return { type: FAILURE_WELCOME_DATA }
+    return { type: FAILURE_WELCOME_DATA }
 }
 
 export function fetchWelcomeData(api) {
-	return (dispatch) => {
-		dispatch(requestWelcomeData());
+    return (dispatch) => {
+        dispatch(requestWelcomeData());
 		// this is to temperally test mock response from backend
-		const data = require('../components/Welcome/__tests__/fixtures/Welcome.json');
-		dispatch(receiveWelcomeData(data));
+        const data = require('../components/Welcome/__tests__/fixtures/Welcome.json');
+        dispatch(receiveWelcomeData(data));
 
 		// return axios.get(api)
 		// 	.then((json) => dispatch(receiveWelcomeData(json.data.response.results)))
 		// 	.catch(() => dispatch(failureWelcomeData()))
-	}
+    }
 }
