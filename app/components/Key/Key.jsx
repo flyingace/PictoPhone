773e25ccde9ccb9b1c121fc86de8f44e2bb10d6f
David Cameron <dc@davidcameron.biz>
2016-05-15 23:12:52 -0400

    Added Upper & Lower case characters and shifting

--- app/components/Key/Key.jsx
+++ app/components/Key/Key.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import './Key.scss';
 
@@ -17,17 +18,47 @@ const Key = React.createClass(/** @lends Key.prototype */{
      * @property {Object} propTypes - An object used to validate props being passed into the components
      */
     propTypes: {
-        letterValue: React.PropTypes.string,
-        onKeyPressed: React.PropTypes.func,
-        addlClass: React.PropTypes.string
+        keyValue: React.PropTypes.array,
+        onKeyPressed: React.PropTypes.func
+    },
+
+    getInitialState() {
+        return this.getStateFromProps(this.props);
+    },
+
+    componentWillReceiveProps(nextProps) {
+        this.setState(this.getStateFromProps(nextProps));
+    },
+
+    getStateFromProps(props) {
+        return {};
     },
 
     handleKeyPressed() {
         if (this.props.onKeyPressed) {
-            this.props.onKeyPressed(this.props.letterValue);
+            this.props.onKeyPressed(this.props.keyValue);
         }
     },
 
+    renderKeyContents() {
+        const additionalClass = this.props.keyValue[0];
+        let keyContents;
+
+        if (this.props.keyValue.length === 1) {
+            keyContents =
+                <div className={"key " + additionalClass} onClick={this.handleKeyPressed}>
+                    <span>{this.props.keyValue[0]}</span>
+                </div>
+        } else {
+            keyContents =
+                <div className="key" onClick={this.handleKeyPressed}>
+                    <span className="character-lc">{this.props.keyValue[0]}</span>
+                    <span className="character-uc">{this.props.keyValue[1]}</span>
+                </div>
+        }
+
+        return keyContents;
+    },
 
     /**
      * Renders the component based on the properties passed in from a parent
@@ -36,10 +67,9 @@ const Key = React.createClass(/** @lends Key.prototype */{
      * @return {ReactElement}
      */
     render() {
-        let keyClass = (this.props.addlClass) ? 'key ' + this.props.addlClass : 'key';
         return (
-            <div className={keyClass} onClick={this.handleKeyPressed}>{this.props.letterValue}</div>
-        );
+            this.renderKeyContents()
+        )
     }
 });
 
