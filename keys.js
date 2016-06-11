5a98002413e5f67eef8d7c47e1c53aa8d1c57a0d
David Cameron <dc@davidcameron.biz>
2016-06-10 21:47:04 -0400

    Add keys.js to repo since repo is now protected

---
+++ keys.js
@@ -0,0 +1,4 @@
+export const FIREBASE_API_KEY = 'AIzaSyA53QLiHClQ6oFSgYC8Adt3bhvd5I1i2Vk';
+export const FIREBASE_AUTH_DOMAIN = 'project-1689613375215710431.firebaseapp.com';
+export const DATABASE_URL = 'https://project-1689613375215710431.firebaseio.com';
+export const STORAGE_BUCKET = 'project-1689613375215710431.appspot.com';
