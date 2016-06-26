3b6dd42b4de10a7e6cb95993787978d85aa1d1a7
lei-clearsky <sportzhulei@gmail.com>
2016-06-26 11:13:46 -0400

    Make lint run parallel, fix all eslint errors and remove unused files

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
-import counter from './counter';
 import welcome from './welcome';
 
 const rootReducer = combineReducers({
-    counter,
     welcome,
     routing
 });
